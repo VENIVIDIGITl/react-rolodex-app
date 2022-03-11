839c70733f7d335668aa70eff79ad7911f5365b7
VeniVidiGiti <99083579+VENIVIDIGITl@users.noreply.github.com>
2022-03-11 18:39:31 +0200

    move card specific code and styling into its own component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import Card from '../card/card.component'
 import './card-list.styles.css';
 
 
@@ -9,16 +10,8 @@ class CardList extends Component {
     return (
       <div className='card-list'>
         {users.map((user) => {
-          const { name, email, id } = user;
           return (
-            <div key={id} className='card-container'>
-              <img 
-                alt={`${name}`}
-                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-              />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
+            <Card user={user} key={user.id} />
           );
         })}
       </div>
