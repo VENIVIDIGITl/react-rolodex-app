a4aae8abbf5c90a1cdd44d3d16c589783cfc58a8
VeniVidiGiti <99083579+VENIVIDIGITl@users.noreply.github.com>
2022-03-11 16:44:42 +0200

    initialize state, add fetch user data from api into componentDidMount()

--- src/App.js
+++ src/App.js
@@ -7,8 +7,19 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      users: [],
+      searchField: '',
+    };
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) => this.setState(() => ({ users })))
+      .catch((error) => console.log(error));
+  }
+
 
   render() {
     return (
